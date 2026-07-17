553146ac2af38c8faf5c4b92449f0b76c7b68037
eltharin <eltharin18@hotmail.fr>
2026-07-17 14:51:52 +0200

    suppr messageactionresolver for base

--- src/DiceRoller/Functions.mjs
+++ src/DiceRoller/Functions.mjs
@@ -1,6 +1,5 @@
 import * as system  from "../_helpers.mjs";
 
-import { MessageActionResolver } from "../ChatMessage/MessageActionResolver.mjs";
 import { AptitudeRoll } from "./Aptitude/AptitudeRoll.mjs";
 
 
@@ -10,16 +9,3 @@ export function registerDiceRolls() {
     CONFIG.Dice.rolls.push(AptitudeRoll);
 }
 
-export function registerMessageEventListener() {
-    Hooks.on("renderChatMessageHTML", (message, html, data) => {
-        html.querySelectorAll(".dice-roll button[data-action]").forEach(btn => {
-            btn.addEventListener("click", event => {
-                const action = event.currentTarget.dataset.action;
-                MessageActionResolver.executeAction(action, event, message, data);
-            });
-        });
-
-    });
-
-    
-}
